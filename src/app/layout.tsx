1f717e0d24c6b55d75464f4ae272e54a1f77fa9c
Ayushi5498 <ayushic383@gmail.com>
2025-06-18 19:49:51 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import { ConvexClientProvider } from "@/providers/convex-client-providers";
+import ConvexClientProvider from "@/providers/convex-client-providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
