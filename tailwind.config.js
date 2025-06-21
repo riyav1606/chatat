089fbe9a4431001787b3695c0e56b266a45a8115
riyav1606 <riyavairale05@gmail.com>
2025-06-21 17:52:27 +0530

    commit 4

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust based on your project structure
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
